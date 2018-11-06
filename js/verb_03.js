6fa06b36f9deaaef4f3ac8be2f6d50e495cd8364
Misty <misty@Mistys-MBP.fios-router.home>
2018-11-05 21:16:16 -0500

    ?

--- js/verb_03.js
+++ js/verb_03.js
@@ -1,15 +1,32 @@
 for(var i=0; i<35; i++) {
 
-	// create a div
 	var shape = document.createElement('div');
-
-	// add class "shape" to div
 	shape.classList.add('shape');
-
-	// select container div
 	var container = document.querySelector('.content');
-
-	// add the div to the DOM
 	container.append(shape);
 
 }
+
+shape.addEventListener('mouseover', function(event){
+    shape.classList.add('shape-interaction');
+  });
+
+  // select container div
+  var container = document.querySelector('.content');
+
+  // add the div to the DOM
+  container.append(shape);
+
+var shapes = document.querySelectorAll('.shape');
+
+shapes.forEach(function(shape, i){
+  shape.addEventListener('mouseover', function(event){
+
+    if (shape.classList.contains('shape-interaction')) {
+      shape.classList.remove('shape-interaction');
+    } else {
+      shape.classList.add('shape-interaction');
+    }
+    
+  });
+});
