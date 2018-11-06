f18fa5152b76ae3d56de2732ba4017aa27e0d499
Misty <misty@Mistys-MBP.fios-router.home>
2018-11-05 20:57:47 -0500

    ahhhh

--- js/verb_03.js
+++ js/verb_03.js
@@ -1,27 +1,27 @@
-function hslColor(h, s, l) {
-  var hue = h;
-  var saturation = s + "%";
-  var luminance = l + "%";
+function hslColor(h, s, l){
+	var hue = h
+	var saturation = s + "%";
+	var luminance = l + "%";
 
-  var color = "hsl(" + hue + "," + saturation + "," + luminance + ")";
+var color = "hsl(" + hue + "," + saturation + "," + luminance + ")";
 
-  return color;
+return color;
 }
 
-for(var i=0; i<200; i++) {
+for(var i=0; i<100; i++) {
 
-	var shape = document.createElement('div');
+	var shapes = document.createElement('div');
 	shape.classList.add('shape');
 	var container = document.querySelector('.content');
 	container.append(shape);
-}
 
+}
 
 var shapes = document.querySelectorAll('.shape');
 
 shapes.forEach(function(shape, i) {
-	var percentage = i / 200 * 100; 
 
-	var color = hslColor(i+180, i, percentage);
-	shape.style.background = color;
-});
+var color = lightblue;
+
+}
+
